432e76c82c344b8806ce527775264e93bb5ab960
Faizan Wani <charmichunkister@gmail.com>
2023-02-09 19:37:07 +0530

    odified sumOfListOfNumbers file

--- Programs/sumOfListOfNumbers.js
+++ Programs/sumOfListOfNumbers.js
@@ -1,18 +1,14 @@
-// // write a function which takes a list of numbers as input and returns its sum:
-// // ps: User spread operator to take input(...)
+// write a function which takes a list of numbers as input and returns its sum:
+// ps: User spread operator to take input(...)
 
-// function sum(...numbers) {
-//   let total = 0;
-//   for (let i = 0; i < numbers.length; i++) {
-//     total = total + numbers[i];
-//   }
-//   return total;
-// }
-// var result = sum(1, 2, 3, 4, 5);
-// console.log(result);
+function sum(...numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total = total + numbers[i];
+  }
+  return total;
+}
+var result = sum(1, 2, 3, 4, 5);
+console.log(result);
 
-// // console.log(sum(1, 2, 3, 4, 5));
-
-var arr = [1, 2, 3];
-var js = arr.reverse();
-console.log(js);
+// console.log(sum(1, 2, 3, 4, 5));
